a269282108a23dba73c49dcc45188a915b181bc7
Brett Chalabian <bchalabian@ucla.edu>
2016-02-28 15:58:03 -0800

    First Commit. Beginning of sender.c made.

---
+++ sender.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+
+void error(char *msg) 
+{
+	perror(msg);
+	exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+	int sockfd, newsockfd, portno, pid;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+
+	if (argc < 2) {
+		 fprintf(stderr,"ERROR, no port provided\n");
+		 exit(1);
+	}
+
+	//reset memory
+	memset((char *) &serv_addr, 0, sizeof(serv_addr));	
+
+	//fill in address info
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	//create UDP socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);	
+	if (sockfd < 0) 
+		error("ERROR opening socket");
+
+
+}
